ea8cd3e6d17f9d669ebcdd352c0480b36e4e30ea
Paul Brooker <pdbrooker@hotmail.com>
2016-05-06 10:02:59 -0300

    Add project files.

---
+++ GrocerCheckRebuild/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GrocerCheckRebuild.Startup))]
+namespace GrocerCheckRebuild
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
